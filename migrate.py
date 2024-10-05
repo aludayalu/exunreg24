6ce0af3c60c3d3d9f1af0535ec4e17be28d180ac
Aarav Dayal <pl46u3.ducklinz@gmail.com>
2024-10-05 16:56:10 +0530

    added migrate script for events

---
+++ migrate.py
@@ -0,0 +1,9 @@
+import litedb, json
+import hashlib
+
+events=litedb.get_conn("events")
+
+data=json.loads(open("data/events.json").read())
+
+for event in data["events"]:
+    events.set(hashlib.sha256(event.encode()).hexdigest(), data["default"])
