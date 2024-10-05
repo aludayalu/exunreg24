5c821561d983fa8774c18ae9b976ddf0853ca06a
Aarav Dayal <pl46u3.ducklinz@gmail.com>
2024-10-05 19:39:58 +0530

    ceiled event ids to first 6 hex characters of its sha256 hash hexdigest

--- migrate.py
+++ migrate.py
@@ -6,4 +6,4 @@ events=litedb.get_conn("events")
 data=json.loads(open("data/events.json").read())
 
 for event in data["events"]:
-    events.set(hashlib.sha256(event.encode()).hexdigest(), data["default"] | {"image":data["events"][event], "name":event})
+    events.set(hashlib.sha256(event.encode()).hexdigest()[:6], data["default"] | {"image":data["events"][event], "name":event})
