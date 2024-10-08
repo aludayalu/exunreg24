c04a0d99cca076286ba391ab85d974d6c244dc94
Aarav Dayal <pl46u3.ducklinz@gmail.com>
2024-10-08 12:34:56 +0530

    migrate script updated and events.json fixed

--- migrate.py
+++ migrate.py
@@ -6,4 +6,7 @@ events=litedb.get_conn("events")
 data=json.loads(open("data/events.json").read())
 
 for event in data["events"]:
-    events.set(hashlib.sha256(event.encode()).hexdigest()[:6], data["default"] | {"image":data["events"][event], "name":event})
+    events.set(hashlib.sha256(event.encode()).hexdigest()[:6], data["default"] | {"image":data["events"][event], "name":event, "descriptions":{
+        "long":data["descriptions"][event]["long"],
+        "short":data["descriptions"][event]["short"]
+    }})
