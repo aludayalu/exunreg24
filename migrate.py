6406c8cbff2e6823d02f9ce1ddab29da69355060
Aarav Dayal <pl46u3.ducklinz@gmail.com>
2024-10-09 10:40:18 +0530

    migrate script updated to add new details from events.json

--- migrate.py
+++ migrate.py
@@ -6,7 +6,16 @@ events=litedb.get_conn("events")
 data=json.loads(open("data/events.json").read())
 
 for event in data["events"]:
-    events.set(hashlib.sha256(event.encode()).hexdigest()[:6], data["default"] | {"image":data["events"][event], "name":event, "descriptions":{
-        "long":data["descriptions"][event]["long"],
-        "short":data["descriptions"][event]["short"]
-    }})
+    events.set(hashlib.sha256(event.encode()).hexdigest()[:6], data["default"] | {"image":data["events"][event],
+        "name":event,
+        "descriptions":{
+            "long":data["descriptions"][event]["long"],
+            "short":data["descriptions"][event]["short"]},
+        "open_to_all":data["open_to_all"][event],
+        "dates":data["dates"][event],
+        "eligibility":data["eligibility"][event],
+        "participants":data["participants"][event],
+        "mode":data["mode"][event],
+        "independant_registration": data["individual"][event],
+        "points":data["points"][event]
+    })
