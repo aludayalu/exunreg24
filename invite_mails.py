9c4b1f087f09f7fee548295cd325cb69479ed7a5
Aarav Dayal <pl46u3.ducklinz@gmail.com>
2024-10-14 12:13:19 +0530

    mail rewritten in html

---
+++ invite_mails.py
@@ -0,0 +1,22 @@
+from monster import render, Flask, escapeString
+import sys, json
+import hashlib, base64
+import resend, secrets_parser
+import litedb, re
+from flask_compress import Compress
+
+resend.api_key = secrets_parser.parse("variables.txt")["RESEND"]
+salt = secrets_parser.parse("variables.txt")["SALT"]
+
+def send_mail(to, subject, html, reply_to="exun@dpsrkp.net"):
+    params = {
+        "from": "Exun Clan <exun@exun.co>",
+        "to": [to],
+        "subject": subject,
+        "html": html,
+        "reply_to": reply_to
+    }
+    email = resend.Emails.send(params)
+    return email
+
+send_mail("aarav@dayal.org", "test", "test")
